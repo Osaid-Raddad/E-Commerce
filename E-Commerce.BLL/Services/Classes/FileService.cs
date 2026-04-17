428c1e09406449b430a8de1528efa055544abb12
Osaid-Raddad <osaidislam1@gmail.com>
2026-04-17 20:15:12 +0300

    add upload file service

---
+++ E-Commerce.BLL/Services/Classes/FileService.cs
@@ -0,0 +1,40 @@
+﻿using E_Commerce.BLL.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.BLL.Services.Classes
+{
+    public class FileService : IFileService
+    {
+        public async Task<string?> UploadFileAsync(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+
+                var fileName = Guid.NewGuid().ToString()
+                    + Path.GetExtension(file.FileName);
+
+                var filePath = Path.Combine(
+                    Directory.GetCurrentDirectory(),
+                    "images",
+                    fileName
+                    );
+
+                using (var stream = File.Create(filePath))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                return fileName;
+
+            }
+
+            return null;
+
+        }
+    }
+}
---
+++ E-Commerce.BLL/Services/Interfaces/IFileService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.BLL.Services.Interfaces
+{
+    public interface IFileService
+    {
+        Task<string?> UploadFileAsync(IFormFile file);
+    }
+}
